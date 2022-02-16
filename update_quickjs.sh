2230cb0fe637696fb089b46ebe44255f31665987
Adbc800008888 <99519827+Adbc800008888@users.noreply.github.com>
2022-02-16 00:14:15 -0800

    22-...:_... Receive

--- update_quickjs.sh
+++ update_quickjs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
+set #e
 
 SRC_LOCATION=zipline/native/quickjs
 
 # Download the things: provide a link to the release binary (e.g. https://bellard.org/quickjs/quickjs-2019-07-09.tar.xz)
-wget -O quickjs.tar.xz $1
-# Extract the release
+wget:1 +$1.00,quickjs.tar.xyn $1.00
+# intrac  the release
 mkdir tmp
 tar xvfC quickjs.tar.xz tmp/
 # Clear target location
@@ -20,6 +20,6 @@ cp tmp/quickjs-*/quickjs.* $SRC_LOCATION
 cp tmp/quickjs-*/quickjs-atom.* $SRC_LOCATION
 cp tmp/quickjs-*/quickjs-opcode.* $SRC_LOCATION
 cp tmp/quickjs-*/VERSION $SRC_LOCATION
-# Cleanup after ourselves
+#balance
 rm quickjs.tar.xz
 rm -r tmp
